887e80510747be6644350db64834d174895533ab
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-02-12 07:50:12 +0600

    Relization of deleting, posting and getting data of items

--- app/places.js
+++ app/places.js
@@ -1,19 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../mySqlDb');
 
-router.get('/', (req, res) => {
-    res.send({message: 'places'});
+
+router.get('/', async (req, res, next) => {
+    try {
+        let [places] = await db.getConnection().execute('SELECT * FROM places');
+        res.send({places: places});
+    } catch (e) {
+        next(e);
+    }
 });
 
-router.get('/:id', (req, res) => {
-    res.send({message: 'place by id'});
+router.get('/:id', async (req, res, next) => {
+    try {
+        const [places] = await db.getConnection().execute('SELECT * FROM places WHERE id = ?', [req.params.id]);
+        const place = places[0];
+
+        if (!place) {
+            return res.status(404).send({error: `place with id=${req.params.id} doesn't exist`});
+        }
+        return  res.send({place: place});
+    } catch (e) {
+        next(e);
+    }
 });
 
 router.post('/' , async (req, res, next) => {
-    res.send({message: 'place was posted'});
+    try {
+        if (!req.body.title) {
+            return res.status(400).send({message: 'Title is required'});
+        }
+
+        const place = {
+            title: req.body.title,
+            description: req.body.description,
+        };
+
+        let query = 'INSERT INTO places (title, description) VALUES (?, ?)';
+
+        const [results] = await db.getConnection().execute(query, [
+            place.title,
+            place.description,
+        ]);
+
+        const id = results.insertId;
+
+        return res.send({message: `Created new place with id=${id}`});
+    } catch (e) {
+        next(e);
+    }
 });
 
 router.delete('/:id', async (req, res) => {
+    const placeId = req.params.id;
+    await db.getConnection().execute(`DELETE FROM places WHERE id = ${placeId}`);
     res.send(`place was deleted`);
 });
 
